3f4e48da536c8f036c978673e64f4e679e2f9f3c
Wesley Tavares <w.souzatavares@gmail.com>
2022-04-03 21:54:39 -0300

    feat: adiciona o layout base estático com formulário

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import 'materialize-css/dist/css/materialize.min.css';
-import 'materialize-css/dist/js/materialize.min.js';
+import Materialize from 'materialize-css/dist/js/materialize.min.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Executa configurações nos componentes Materialize
+document.addEventListener('DOMContentLoaded', function () {
+  const elements = document.querySelectorAll('.modal');
+  Materialize.Modal.init(elements, {}); // https://materializecss.com/modals.html#initialization
+
+  Materialize.updateTextFields(); // https://materializecss.com/text-inputs.html#input
+});
 
 ReactDOM.render(
   <React.StrictMode>
