a091fe0df2708a63c3be69f95702e3175dcfec8d
Wesley Tavares <w.souzatavares@gmail.com>
2022-04-03 21:58:32 -0300

    refactor: remove teste inútil para o layout básico
    Era um teste apenas para garantir que estava tudo configurado certo. No futuro deve adicionar novos testes para os componentes principais (pelo menos).

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('Renderiza h3 com a mensagem "Hello Universe!"', () => {
-  render(<App />);
-  const h3Element = screen.getByText(/Hello Universe!/i);
-  expect(h3Element).toBeInTheDocument();
-});
