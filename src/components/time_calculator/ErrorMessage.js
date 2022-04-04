7dc4d60ef08493bf672d988ae156be33d01b8581
Wesley Tavares <w.souzatavares@gmail.com>
2022-04-04 20:34:30 -0300

    fix: corrige o posicionamento da mensagem de erro na calculadora

--- src/components/time_calculator/ErrorMessage.js
+++ src/components/time_calculator/ErrorMessage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ErrorMessage(props) {
   return (
-    <div className="col red-text text-darken-4 center-align" style={{ marginTop: "16px" }}>
+    <div className="col s12 red-text text-darken-4 center-align" style={{ marginTop: "16px" }}>
       <b>{props.message}</b>
     </div>
   );
